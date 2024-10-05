a3f844fcb04282c6fe01c616b2ff708bf9dc1a82
vitly21 <salnikovvityavitya1@yandex.ru>
2024-10-05 13:22:38 +0500

    + homework 4

---
+++ homework4/zadanie 4.py
@@ -0,0 +1,7 @@
+print("ведите значения a, b, c: ")
+a = int(input())
+b = int(input())
+c = int(input())
+x = 7*b + 2*c - a
+print(f"Решение уравнения x = 7*b + 2*c - a с введенными значениями a, b, c равно {x}")
+
